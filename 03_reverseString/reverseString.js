a39dadde03ac332a06b497cb897f500b412daa83
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-10 15:53:06 -0600

    Update

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let stringToBeReversed = str;
+    let newString = "";
+    for (let i = stringToBeReversed.length; i > 0; i--) {
+        newString += stringToBeReversed[i-1];
+    }
+    return newString;
 };
 
 // Do not edit below this line
