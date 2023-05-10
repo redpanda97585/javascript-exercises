2931579d9775cc318c0bb98803f355ba801fbf85
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-10 14:29:36 -0600

    Update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let strVariable = str;
+    let repetitions = num;
+    if (repetitions === 0) {
+        return '';
+    } else if (repetitions < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 1; i < repetitions; i++) {
+            strVariable += str
+        }
+        return strVariable;
+    }
 };
 
 // Do not edit below this line
