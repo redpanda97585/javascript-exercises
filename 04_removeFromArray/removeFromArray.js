ecb3a67d4a4a4a43ff62dcad1a1bc80880821886
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-10 22:02:29 -0600

    Update

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let givenArray = array;
+    let restParameters = args;  
+    let newArray = givenArray.filter(item => !restParameters.includes(item));
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
