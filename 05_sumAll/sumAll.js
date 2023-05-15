9387f36d8b446761c5260fc651608d3a784f2f8d
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-15 16:31:37 -0600

    Update

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let total = 0;
+    if (start > end) {
+        end = start;
+        start = 1;
+    } else if (start < 0) {
+        return 'ERROR';
+    } else if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    };
+    for (let i = start; i < end+1; i++) {
+        total += i;
+    };
+    return total;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// in the test03 i need to change
+// the second and third parameters in the loop
