b5702859691d5e6fceab8cbdeb69a88f70a8168e
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-05-15 22:54:23 -0600

    Update

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    if (givenYear % 4 === 0) {
+        if (givenYear % 100 === 0) {
+            if (givenYear % 100 === 0 && givenYear % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            };
+        } else {
+            return true;
+        };
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
