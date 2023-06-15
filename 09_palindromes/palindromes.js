58dacf4e813239baa7f3e74db7e9261c9b62e480
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-06-14 21:20:58 -0600

    Complete, there is a bug in one of the tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (givenWord) {
+    const original = Array.from(givenWord);
+    const reversed = Array.from(givenWord).reverse();
 
+    original.forEach(e => {
+        const index = original.indexOf(e);
+        original[index] = original[index].toLowerCase();
+        if (e === " " || e === '!' || e === '.' || e === ',') {
+            const index = original.indexOf(e);
+            original.splice(index,1);
+        };
+    });
+    reversed.forEach(e => {
+        const index = reversed.indexOf(e);
+        reversed[index] = reversed[index].toLowerCase();
+        if (e === " " || e === ','|| e === '!' || e === '.') {
+            const index = reversed.indexOf(e);
+            reversed.splice(index,1);
+        };
+    });
+
+    if (JSON.stringify(original) === JSON.stringify(reversed)) {
+        return true;
+    } else {
+        return false;
+    };
+    // indexOf()
+    // splice(index, 1)
 };
 
 // Do not edit below this line
