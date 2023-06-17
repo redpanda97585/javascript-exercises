19a71e9a239f66061c79d6d494c5e5eeaef280f9
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-06-17 15:34:29 -0600

    Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(givenNumber) {
+    if (givenNumber < 0) return "OOPS";
+    if (givenNumber === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < givenNumber; i++) {
+        const ab = b;
+        b = a + b;
+        a = ab;
+        // console.log(ab);
+    };
+    return b;
 };
 
 // Do not edit below this line
