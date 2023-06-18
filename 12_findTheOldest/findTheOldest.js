5fd573ab05bfc3760310a3043d062306e31df219
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-06-17 20:29:05 -0600

    Complete, unexpected error trying to declare the getAge function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(givenArray) {
+    return givenArray.reduce((oldest, currentPerson) => {
+        const oldestAge = oldest.getAge(
+            oldest.yearOfDeath - oldest.yearOfBirth);
+        const currentPersonAge = currentPerson.getAge(
+            currentPerson.yearOfDeath - currentPerson.yearOfBirth);
+        return oldestAge < currentPersonAge ? currentPerson : oldest;
+    });
 };
+
+const getAge = function (death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    };
+    return death - birth;
+};
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
